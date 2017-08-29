1e525c99324bb96b0b824811239f06847911d0a1
saq9516 <31365464+saq9516@users.noreply.github.com>
2017-08-29 13:39:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -61,36 +61,12 @@ var htmlTemplate = `
 `;
 return htmlTemplate;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get("/article-one", function(req,res){
-res.send(createTemplate(articleOne))
+res.send(createTemplate(articleOne));
 });
 
 app.get("/article-two", function(req,res){
