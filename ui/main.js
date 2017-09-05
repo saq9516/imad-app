5646555235426b124736f54da404098b3caaa388
saq9516 <31365464+saq9516@users.noreply.github.com>
2017-09-05 19:23:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,10 +9,20 @@ element.innerHTML='New value';
 
 
 var img=document.getElementById('madi');
+var marginLeft=0;
+function moveRight{
+    marginLeft=marginLeft+10;
+    img.style.marginLeft= marginLeft + 'px';
+    
+    
+}
+
+
+
 img.onclick=function()
 {
-    
-    img.style.marginLeft='100px';
+    var interval=setInterval(moveRight,100);
+    // img.style.marginLeft='100px';
     
     
     
